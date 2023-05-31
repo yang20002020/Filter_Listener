8c4a1c713fa3274e2ca24c31d5ad746542300c2d
yang20002020 <2628636987@qq.com>
2023-05-31 20:42:40 +0800

    统计当前在线人数的案例

--- ListenerDemo5/src/main/java/onlinecount/OnlineCountServletContextListener.java
+++ ListenerDemo5/src/main/java/onlinecount/OnlineCountServletContextListener.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletContextListener;
 public class OnlineCountServletContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-         //在服务器启动的时候初始化一个值为0
+        //在服务器启动的时候初始化一个值为0
         //还需要将这个值存入到ServletContext中
         servletContextEvent.getServletContext().setAttribute("count",0);
     }
