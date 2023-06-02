5f63e8fa8db00f93af08db7a087aefe8d472f8d8
yang20002020 <2628636987@qq.com>
2023-06-02 22:44:49 +0800

    案例：权限验证过滤器，没有过滤器的时候会出现直接登录成功的效果

---
+++ web05_login/src/main/java/fuyu/model/UserModel.java
@@ -0,0 +1,21 @@
+package fuyu.model;
+
+import fuyu.domain.User;
+import fuyu.utils.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.SQLException;
+
+/*
+用户的模型层
+ */
+public class UserModel {
+    public User login(User user) throws SQLException {
+
+        QueryRunner queryRunner=new QueryRunner(JDBCUtils.getDataSource());
+        String sql="select * from user where username= ? and password = ?";
+        User existUser=queryRunner.query(sql,new BeanHandler<User>(User.class),user.getUsername(),user.getPassword());
+        return existUser;
+    }
+}
---
+++ web05_login/target/web05_login-1.0-SNAPSHOT/jsp/sucess.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+   <h1> 您已经登录成功！欢迎:${existUser.username}</h1>
+</body>
+</html>
